fd39599c1bfeb867cd52be679407578cd0f87480
0Notzura <111460844+0Notzura@users.noreply.github.com>
2024-09-12 09:07:45 -0300

    Add files via upload

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-const apiUrl = 'http://backend.example.com/api'; 
+const apiUrl = 'http://backend.example.com';
 
   const authOptions = document.getElementById('authOptions');
   const authOptionsRegister = document.getElementById('showRegisterForm');
